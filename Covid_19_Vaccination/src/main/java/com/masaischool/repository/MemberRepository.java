8e954d6bbe066b6d26d7489af545c7bb95988a4d
sawantaviraj2 <sawantaviraj2@gmail.com>
2023-08-25 14:12:18 +0530

    VaccinService and VaccinServiceImpl added

--- Covid_19_Vaccination/src/main/java/com/masaischool/repository/MemberRepository.java
+++ Covid_19_Vaccination/src/main/java/com/masaischool/repository/MemberRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.masaischool.entity.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 	@Query("SELECT m FROM Member m JOIN m.user u WHERE u.aadharNo=:aadharN")
--- Covid_19_Vaccination/src/main/java/com/masaischool/service/SerImpl/AppointmentServiceImpl.java
+++ Covid_19_Vaccination/src/main/java/com/masaischool/service/SerImpl/AppointmentServiceImpl.java
@@ -13,36 +13,43 @@ import com.masaischool.repository.VaccinationCenterRepository;
 import com.masaischool.service.AppointmentService;
 
 @Service
-public class AppointmentServiceImpl implements AppointmentService{
-	
-	
+public class AppointmentServiceImpl implements AppointmentService {
+
 	// Injects a dependency into the class
-	@Autowired 
 	private AppointmentRepository appointmentRepository;
-	
-	@Autowired
 	private MemberRepository memberRepository;
-	
-	@Autowired
 	private VaccinationCenterRepository vaccinationCenterRepository;
+
+	@Autowired
+	public AppointmentServiceImpl(AppointmentRepository appointmentRepository, MemberRepository memberRepository,
+			VaccinationCenterRepository vaccinationCenterRepository) {
+		super();
+		this.appointmentRepository = appointmentRepository;
+		this.memberRepository = memberRepository;
+		this.vaccinationCenterRepository = vaccinationCenterRepository;
+	}
 
 	@Override
 	public Appointment getAppointmentDetails(Integer bookingId) {
 		return appointmentRepository.findById(bookingId)
-                .orElseThrow(() -> new AppointmentNotFoundException("Appointment not found with ID: " + bookingId));
+				.orElseThrow(() -> new AppointmentNotFoundException("Appointment not found with ID: " + bookingId));
 	}
 
 	@Override
 	public Appointment addAppointment(Integer memberId, Integer vaxCenterId, Appointment appointment) {
-    if(appointment == null) throw new AppointmentNotFoundException("Appointment couldn't saved");
-		
-		Member member = memberRepository.findById(memberId).orElseThrow(()-> new AppointmentNotFoundException("Please Provide a valid MemberId to book an Appointment"));
-		VaccinationCenter vaxCenter = vaccinationCenterRepository.findById(vaxCenterId).orElseThrow(()-> new AppointmentNotFoundException("Please Provide a valid Vaccination center id to book an Appointment"));
-		
+		if (appointment == null)
+			throw new AppointmentNotFoundException("Appointment couldn't saved");
+
+		Member member = memberRepository.findById(memberId).orElseThrow(
+				() -> new AppointmentNotFoundException("Please Provide a valid MemberId to book an Appointment"));
+		VaccinationCenter vaxCenter = vaccinationCenterRepository.findById(vaxCenterId)
+				.orElseThrow(() -> new AppointmentNotFoundException(
+						"Please Provide a valid Vaccination center id to book an Appointment"));
+
 		member.setAppointment(appointment);
-		
+
 		vaxCenter.getAppointments().add(appointment);
-		
+
 		appointment.setMemberId(member);
 		appointment.setVaxCenter(vaxCenter);
 		appointment.setSlot(Slot.SLOT1);
@@ -52,17 +59,21 @@ public class AppointmentServiceImpl implements AppointmentService{
 
 	@Override
 	public Appointment updateAppointment(Integer bookingId, Appointment appointment) {
-		if(bookingId==null) throw new AppointmentNotFoundException("Please provide Appointment id you want to update");
-		if(appointment==null) throw new AppointmentNotFoundException("Please provide Updated Appointment details");
+		if (bookingId == null)
+			throw new AppointmentNotFoundException("Please provide Appointment id you want to update");
+		if (appointment == null)
+			throw new AppointmentNotFoundException("Please provide Updated Appointment details");
 		appointment.setBookingId(bookingId);
 		return appointmentRepository.save(appointment);
 	}
 
 	@Override
 	public Appointment deleteAppointment(Integer bookingId) {
-		if(bookingId==null) throw new AppointmentNotFoundException("Please provide Appointment id you want to delete");
+		if (bookingId == null)
+			throw new AppointmentNotFoundException("Please provide Appointment id you want to delete");
 		Optional<Appointment> optional = appointmentRepository.findById(bookingId);
-		if(optional.isEmpty()) throw new AppointmentNotFoundException("Appointment Not Found");
+		if (optional.isEmpty())
+			throw new AppointmentNotFoundException("Appointment Not Found");
 		Appointment appointment = optional.get();
 		appointmentRepository.delete(appointment);
 		return appointment;
