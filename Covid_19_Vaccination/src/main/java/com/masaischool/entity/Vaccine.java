1d8c440861c9aa2de94381a7b581fc89082d8237
Aviraj Sawant <121309811+Sawantaviraj2@users.noreply.github.com>
2023-08-28 15:42:54 +0530

    Update Vaccine update

--- Covid_19_Vaccination/src/main/java/com/masaischool/entity/Vaccine.java
+++ Covid_19_Vaccination/src/main/java/com/masaischool/entity/Vaccine.java
@@ -8,10 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vaccine {
 
 	@Id
@@ -24,7 +28,14 @@ public class Vaccine {
 	@OneToOne(cascade = CascadeType.ALL)
 	private Member member;
 
+	@JsonIgnore
 	@OneToOne(mappedBy = "vaccine", cascade = CascadeType.ALL)
 	private VaccineCount vaccineCount;
+
+	public Vaccine(String vaxName, String description) {
+		super();
+		this.vaxName = vaxName;
+		this.description = description;
+	}
 
 }
